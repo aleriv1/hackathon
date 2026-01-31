04997add7644b6a70d4ba1c6455a0c9b94ad88cf
Ivan <ivansaigin04@mail.ru>
2026-01-31 19:01:58 +0300

    creating module logic for shapes

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,94 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
+import { randomSize, randomHex, angleColor, radialGradient, conicGradient, randomShape } from "../utils"
 
 export class ShapeModule extends Module {
+
+    constructor() {
+        super('shape', 'Случайная фигура')
+        this.isActive = false
+        this.clickBody = this.handleClick.bind(this)
+    }
+
+    trigger() {
+        this.isActive = !this.isActive
+
+        if (this.isActive) {
+            alert("После того как вы нажали данный пункт меню, вам доступен функционал неограниченного создания фиугр при клике на документ\nДля выхода из этого режима Нажмите еще раз на данный пункт меню")
+            document.addEventListener("click", this.clickBody)
+        } else {
+            alert("К сожалению что-то пошло не так, нажмите пункт меню еще раз")
+            document.removeEventListener("click", this.clickBody)
+            this.cleanup()
+        }
+    }
+    handleClick(event) {
+        if (event.target.closest(".random-figure") || event.target.closest(".menu-item")) return
+        this.createFigure(event.clientX, event.clientY)
+
+    }
+    createFigure(x, y) {
+
+        const figure = document.createElement("div")
+        figure.className = "random-figure"
+        const color = this.randomTypeColor()
+        const size = randomSize(20, 150)
+        const shapeForm = randomShape()
+
+        figure.style.cssText = `
+        position: 'fixed'
+        width: ${size.width}px,
+        height: ${size.height}px,
+        left: ${x - size.width / 2}px,
+        top: ${y - size.height / 2}px,
+        background: ${color},
+        pointerEvents: 'none',
+        zIndex: ${Math.floor(Math.random() * 100)},
+        boxShadow: '0 4px 15px rgba(0,0,0,0.2)'
+        `
+
+        this.ShapeStyle(figure, shapeForm, size)
+
+        document.body.appendChild(figure)
+
+    }
+
+    ShapeStyle(figure, shapeForm, size) {
+        switch (shapeForm) {
+            case 'circle':
+                figure.style.borderRadius = '50%'
+                break
+
+            case 'square':
+                figure.style.borderRadius = '0'
+                break
+
+            case 'rounded':
+                figure.style.borderRadius = '25%'
+                break
+
+            case 'ellipse':
+                figure.style.borderRadius = '50%'
+                const newHeight = size.height * 0.7
+                figure.style.height = `${newHeight}px`
+                break
+        }
+    }
+
+    randomTypeColor() {
+        const options = [
+            () => randomHex(),
+            () => angleColor(5),
+            () => angleColor(2),
+            () => angleColor(7),
+            () => radialGradient(),
+            () => conicGradient()
+        ]
+        const randomOptionColor = options[Math.floor(Math.random() * options.length)]
+        return randomOptionColor()
+    }
+
+    cleanup() {
+        document.querySelectorAll('.random-figure').forEach(fig => fig.remove())
+    }
 
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,62 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export function randomSize(min, max) {
+  const width = Math.floor(Math.random() * (max - min + 1)) + min
+  const height = Math.floor(Math.random() * (max - min + 1)) + min
+  return { width, height }
+}
+export function randomHex() {
+  const hex = "0123456789ABCDEF"
+  let color = "#"
+
+  for (i = 0; i <= 8; i++) {
+    color += hex[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+export function angleColor(count = 2) {
+  const color = []
+
+  for (i = 0; i <= count; i++) {
+    color.push(randomHex())
+  }
+  const angle = Math.floor(Math.random() * 360)
+  return `linear-gradient(${angle}deg, ${color.join(", ")})`
+}
+
+export function radialGradient() {
+  const color1 = randomHex()
+  const color2 = randomHex()
+
+  const positions = [
+    'center',
+    'top left',
+    'top right',
+    'bottom left',
+    'bottom right',
+    `${Math.random() * 100}% ${Math.random() * 100}%`
+  ]
+
+  const position = positions[Math.floor(Math.random() * positions.length)]
+  const shape = Math.random() > 0.5 ? 'circle' : 'ellipse'
+
+  return `radial-gradient(${shape} at ${position}, ${color1}, ${color2})`
+}
+
+export function conicGradient() {
+
+  const color = []
+  const countColor = 2 + Math.floor(Math.random() * 4)
+  for (i = 0; i <= countColor; i++) {
+    color.push(randomHex())
+  }
+  return `conic-gradient(${color.join(", ")})`
+}
+
+export function randomShape() {
+  const shapes = ['circle', 'square', 'rounded', 'ellipse']
+  return shapes[Math.floor(Math.random() * shapes.length)]
+}
