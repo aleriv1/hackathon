a4fc2e9eacd7e9fa41c5ef9239e7a9ef9917cc30
EdwardGal <edwgit@gmail.com>
2026-01-31 14:14:52 +0300

    Implemented background module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,34 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
 
 export class BackgroundModule extends Module {
+  constructor() {
+    super('background', 'Случайный фон');
+    this.$body = document.body;
+    this.color = localStorage.getItem('appBgColor');
+  }
 
+  trigger() {
+    if (this.color) {
+      const chooseNew = confirm('Хотите выбрать новый цвет?');
+      if (chooseNew) {
+        this.color = this.getRandomHexColor();
+      }
+    } else {
+      this.color = this.getRandomHexColor();
+    }
+
+    this.$body.style.backgroundColor = this.color;
+    localStorage.setItem('appBgColor', this.color);
+  }
+
+  getRandomHexColor() {
+    const hex = '0123456789abcdef';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+  }
 }
