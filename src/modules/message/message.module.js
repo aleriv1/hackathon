5bf3d604dcef85632d8edcacbce93ab3f0caf097
Aleksandr <aleksandr_ermakov@outlook.com>
2026-02-01 19:25:27 +0300

    Anastasiya's wish

--- src/modules/message/message.module.js
+++ src/modules/message/message.module.js
@@ -3,44 +3,43 @@ import { getRandomColor, random } from '../../utils';
 import styles from './message.module.css';
 
 export default class MessageModule extends Module {
+    static messages = [
+        'Прекрасно выглядите!',
+        'Самое лучшее случайное сообщение!',
+        'Самое худшее случайное сообщение!',
+        'Какой сегодня хороший день!',
+        'Как дела?',
+        'JS - твой самый любимый язык программирования',
+        'Когда уже REACT?',
+        'Пейте больше воды!',
+        'Сделай 30 отжиманий',
+        'Выпрями спину!',
+    ];
+
     constructor(type, text) {
         super(type, text)
     }
 
-    getRandomMessage() {
-        const messages = [
-            'Прекрасно выглядите!',
-            'Самое лучшее случайное сообщение!',
-            'Самое худшее случайное сообщение!',
-            'Какой сегодня хороший день!',
-            'Как дела?',
-            'JS - твой самый любимый язык программирования',
-            'Когда уже REACT?',
-            'Пейте больше воды!',
-            'Сделай 30 отжиманий',
-            'Выпрями спину!',
-        ]
-        return messages[random(0, messages.length - 1)]
-    }
-
-    createMessageElement(text, color) {
-        const messageElement = document.createElement('p')
-        messageElement.className = styles['message-element'];
-        messageElement.textContent = text
-        messageElement.style.background = color
-        document.body.append(messageElement)
-        return messageElement
-    }
-
     trigger() {
-        const messageText = this.getRandomMessage()
-        const colorBackground = getRandomColor()
+        const resultOfRandomNumber = random(0, MessageModule.messages.length - 1);
+        const resultOfRandomColor = getRandomColor();
 
-        const message = this.createMessageElement(messageText, colorBackground)
 
+        const result = MessageModule.messages[resultOfRandomNumber];
+
+        const messageElement = document.createElement('div');
+        messageElement.className = styles['message-element'];
+        messageElement.textContent = result;
+        messageElement.style.backgroundColor = resultOfRandomColor;
+        document.body.appendChild(messageElement);
+
+        this.removeElementAfterTimeOut(messageElement, 3000);
+    }
+
+    removeElementAfterTimeOut(element, timeout) {
         function removeElement() {
-            message.remove()
+            element.remove()
         }
-        setTimeout(removeElement, 3000)
+        setTimeout(removeElement, timeout)
     }
 }
