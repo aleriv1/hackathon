5645faa29498670566f7dcfface00df902f0c21a
Aleksandr <aleksandr_ermakov@outlook.com>
2026-02-01 13:43:37 +0300

    message fix refactor

--- src/modules/message/message.module.js
+++ src/modules/message/message.module.js
@@ -8,7 +8,6 @@ export default class MessageModule extends Module {
     }
 
     getRandomMessage() {
-
         const messages = [
             'Прекрасно выглядите!',
             'Самое лучшее случайное сообщение!',
@@ -21,7 +20,6 @@ export default class MessageModule extends Module {
             'Сделай 30 отжиманий',
             'Выпрями спину!',
         ]
-
         return messages[random(0, messages.length - 1)]
     }
 
@@ -35,40 +33,6 @@ export default class MessageModule extends Module {
     }
 
     trigger() {
-
-        //массив случайных сообщений
-        // const messages = [
-        //     'Прекрасно выглядите!',
-        //     'Самое лучшее случайное сообщение!',
-        //     'Самое худшее случайное сообщение!',
-        //     'Какой сегодня хороший день!',
-        //     'Как дела?',
-        //     'JS - твой самый любимый язык программирования',
-        //     'Когда уже REACT?',
-        //     'Пейте больше воды!',
-        //     'Сделай 30 отжиманий',
-        //     'Выпрями спину!',
-        // ]
-
-        //импортированная функция, дает рандомное число из указанных
-        // const resultOfRandomNumber = random(0, 19)
-        // const resultOfRandomNumber = random(0, messages.length - 1)
-
-        //импортированная функция, дает рандомный цвет
-        // const resultOfRandomColor = getRandomColor()
-
-
-        // const result = messages.find((item, index) => {
-        //     return index === resultOfRandomNumber
-        // })
-
-
-        // const messageElement = document.createElement('div');
-        // messageElement.className = 'message-element';
-        // messageElement.innerText = result;
-        // messageElement.style.background = resultOfRandomColor;
-        // document.body.appendChild(messageElement);
-
         const messageText = this.getRandomMessage()
         const colorBackground = getRandomColor()
 
