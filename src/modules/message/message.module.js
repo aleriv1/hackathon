40ef646d9c6a0a323de240c5ac0a1cad575c6190
Aleksandr <aleksandr_ermakov@outlook.com>
2026-02-01 13:26:39 +0300

    message fix

--- src/modules/message/message.module.js
+++ src/modules/message/message.module.js
@@ -7,9 +7,8 @@ export default class MessageModule extends Module {
         super(type, text)
     }
 
-    trigger() {
+    getRandomMessage() {
 
-        //массив случайных сообщений
         const messages = [
             'Прекрасно выглядите!',
             'Самое лучшее случайное сообщение!',
@@ -23,24 +22,60 @@ export default class MessageModule extends Module {
             'Выпрями спину!',
         ]
 
+        return messages[random(0, messages.length - 1)]
+    }
+
+    createMessageElement(text, color) {
+        const messageElement = document.createElement('p')
+        messageElement.className = 'message-element';
+        messageElement.textContent = text
+        messageElement.style.background = color
+        document.body.append(messageElement)
+        return messageElement
+    }
+
+    trigger() {
+
+        //массив случайных сообщений
+        // const messages = [
+        //     'Прекрасно выглядите!',
+        //     'Самое лучшее случайное сообщение!',
+        //     'Самое худшее случайное сообщение!',
+        //     'Какой сегодня хороший день!',
+        //     'Как дела?',
+        //     'JS - твой самый любимый язык программирования',
+        //     'Когда уже REACT?',
+        //     'Пейте больше воды!',
+        //     'Сделай 30 отжиманий',
+        //     'Выпрями спину!',
+        // ]
+
         //импортированная функция, дает рандомное число из указанных
         // const resultOfRandomNumber = random(0, 19)
-        const resultOfRandomNumber = random(0, messages.length - 1)
+        // const resultOfRandomNumber = random(0, messages.length - 1)
+
         //импортированная функция, дает рандомный цвет
-        const resultOfRandomColor = getRandomColor()
+        // const resultOfRandomColor = getRandomColor()
 
 
-        const result = messages.find((item, index) => {
-            return index === resultOfRandomNumber
-        })
-        const messageElement = document.createElement('div');
-        messageElement.className = 'message-element';
-        messageElement.innerText = result;
-        messageElement.style.background = resultOfRandomColor;
-        document.body.appendChild(messageElement);
+        // const result = messages.find((item, index) => {
+        //     return index === resultOfRandomNumber
+        // })
+
+
+        // const messageElement = document.createElement('div');
+        // messageElement.className = 'message-element';
+        // messageElement.innerText = result;
+        // messageElement.style.background = resultOfRandomColor;
+        // document.body.appendChild(messageElement);
+
+        const messageText = this.getRandomMessage()
+        const colorBackground = getRandomColor()
+
+        const message = this.createMessageElement(messageText, colorBackground)
 
         function removeElement() {
-            messageElement.remove()
+            message.remove()
         }
         setTimeout(removeElement, 3000)
     }
