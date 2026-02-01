bc92c484c80f4484569611f51971b8b8c70c3b34
Aleksandr <aleksandr_ermakov@outlook.com>
2026-02-01 03:35:32 +0300

    module timer in the menu

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import './styles.css'
 import { ContextMenu } from './menu'
+
 import MessageModule from './modules/message/message.module'
+
 import { SnowModule } from './modules/snowFlakes.module'
-import { ClicksModule } from './modules/clicks.module'
 import { BackgroundModule } from './modules/background.module'
+import { TimerModule } from './modules/timer.module'
+
+import { ClicksModule } from './modules/clicks.module'
+
 import { ShapeModule } from './modules/shape.module'
 
 const menu = new ContextMenu('.menu')
@@ -14,5 +19,6 @@ menu.add(new ClicksModule('Считать клики'))
 
 menu.add(new SnowModule())
 menu.add(new BackgroundModule())
+menu.add(new TimerModule())
 
 menu.add(new ShapeModule())
